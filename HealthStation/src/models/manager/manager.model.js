fddfdcd55ba94c3c465eb803b847a02a026a7d82
Văn Thế Vinh <ewsxztrfvcuyhnb@gmail.com>
2022-01-02 05:40:15 +0700

    User/ Admin/ Manager modelsl

---
+++ HealthStation/src/models/manager/manager.model.js
@@ -0,0 +1,32 @@
+const { db, pgp } = require('../../config/db');
+const userM = require('../user/user.model');
+class ManagerModel {
+  account_tb = new pgp.helpers.TableName({ table: 'Account' });
+  async createManager(username) {
+    const user_manager = {
+      username: username,
+      password: '',
+      permission: 2,
+    };
+    const rs = await userM.create(user_manager);
+  }
+  async updatePasswordById(id, password) {
+    const rs = userM.updatePasswordById(id, password);
+  }
+  async blockManagerWithId(id) {
+    const new_permission = 3;
+    const user = await userM.findById(id);
+    if (user.data.permission == 2) {
+      const queryString = `
+       update $(table) set permission = $(new_permission) where account_id = $(id)
+    `;
+      await db.none(queryString, {
+        table: this.account_tb,
+        new_permission,
+        id,
+      });
+    }
+  }
+}
+
+module.exports = new ManagerModel();
