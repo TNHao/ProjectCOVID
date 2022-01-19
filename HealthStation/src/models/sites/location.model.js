a55a2e4744f88f3ecc35e589f07bfaba7e7d8ab2
Văn Thế Vinh <ewsxztrfvcuyhnb@gmail.com>
2022-01-19 18:42:38 +0700

    Update manager add, view patient

--- HealthStation/src/models/sites/location.model.js
+++ HealthStation/src/models/sites/location.model.js
@@ -1,63 +1,72 @@
-const { db, pgp } = require('../../config/db')
+const { db, pgp } = require('../../config/db');
 
 class quarantineLocation {
-    table = new pgp.helpers.TableName({ table: "Quarantine_Location" });
-    async findAll() {
-        const data = await db.any(`select * from $1 order by location_id`, this.table)
-        return { data }
-    }
-    async findById(id) {
-        const data = await db.one('select * from ${table} where location_id = ${id}', {
-            table: this.table,
-            id: id,
-        });
-        return { data };
-    }
-    async create(location) {
-        const queryString =
-            `
+  table = new pgp.helpers.TableName({ table: 'Quarantine_Location' });
+  async findAll() {
+    const data = await db.any(
+      `select * from $1 order by location_id`,
+      this.table
+    );
+    return { data };
+  }
+  async findById(id) {
+    const data = await db.one(
+      'select * from ${table} where location_id = ${id}',
+      {
+        table: this.table,
+        id: id,
+      }
+    );
+    return { data };
+  }
+  async create(location) {
+    const queryString = `
         insert into $(table)(name,num_patients, capacity)
         values($(name), $(num_patients), $(capacity));
         `;
-        await db.none(queryString, {
-            table: this.table,
-            name: location.name,
-            num_patients: location.num_patients,
-            capacity: location.capacity
-        });
-    }
-
-    async deleteById(id) {
-
-        const queryString =
-            `delete from $(table) where location_id=$(id)`;
-        await db.none(queryString, {
-            table: this.table,
-            id
-        });
-    }
-    async findByLocationId(id) {
-        const queryString =
-            `select * from $(table) where location_id=$(id)`;
-        const data = await db.oneOrNone(queryString, {
-            table: this.table,
-            id
-        })
-        return { data };
-    }
-    async update(location) {
-        const queryString = `
+    await db.none(queryString, {
+      table: this.table,
+      name: location.name,
+      num_patients: location.num_patients,
+      capacity: location.capacity,
+    });
+  }
+  async findAvailableLocation() {
+    const data = await db.any(
+      'select * from ${table} where "num_patients" < "capacity"',
+      {
+        table: this.table,
+      }
+    );
+    return { data };
+  }
+  async deleteById(id) {
+    const queryString = `delete from $(table) where location_id=$(id)`;
+    await db.none(queryString, {
+      table: this.table,
+      id,
+    });
+  }
+  async findByLocationId(id) {
+    const queryString = `select * from $(table) where location_id=$(id)`;
+    const data = await db.oneOrNone(queryString, {
+      table: this.table,
+      id,
+    });
+    return { data };
+  }
+  async update(location) {
+    const queryString = `
        update $(table) set name = $(name),num_patients = $(num_patients),capacity = $(capacity) where location_id = $(id)
     `;
-        await db.none(queryString, {
-            table: this.table,
-            id: location.location_id,
-            name: location.name,
-            num_patients: location.num_patients,
-            capacity: location.capacity
-        });
-    }
+    await db.none(queryString, {
+      table: this.table,
+      id: location.location_id,
+      name: location.name,
+      num_patients: location.num_patients,
+      capacity: location.capacity,
+    });
+  }
 }
 
-
-module.exports = new quarantineLocation()
+module.exports = new quarantineLocation();
--- HealthStation/src/routes/manager.route.js
+++ HealthStation/src/routes/manager.route.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const upload = require('../middlewares/multer.middleware')
+const upload = require('../middlewares/multer.middleware');
 const managerController = require('../controllers/manager/manager.controller');
 
 router.get('/', managerController.get);
 router.get('/account-management', managerController.getAccount);
 router.get('/account-management/:id', managerController.getAccountDetails);
 router.get('/account-management/:id/edit', managerController.getAccountEdit);
+router.post('/', managerController.addPatient);
 
 router.get('/category-management', managerController.getCategory);
 router.post('/category-management', managerController.createCategory);
@@ -15,9 +16,17 @@ router.delete('/category-management', managerController.deleteCategory);
 
 router.get('/product-management', managerController.getProduct);
 router.get('/product-management/create', managerController.getCreateProduct);
-router.post('/product-management/create', upload.array('images', 5), managerController.postCreateProduct);
+router.post(
+  '/product-management/create',
+  upload.array('images', 5),
+  managerController.postCreateProduct
+);
 router.get('/product-management/:id', managerController.detailsProduct);
-router.post('/product-management/:id',upload.array('images', 5), managerController.updateProduct);
+router.post(
+  '/product-management/:id',
+  upload.array('images', 5),
+  managerController.updateProduct
+);
 router.delete('/product-management', managerController.deleteProduct);
 
 router.get('/package-management', managerController.getPackage);
@@ -26,6 +35,5 @@ router.post('/package-management/create', managerController.postCreatePackage);
 router.get('/package-management/:id', managerController.detailsPackage);
 router.post('/package-management/:id', managerController.updatePackage);
 router.delete('/package-management', managerController.deletePackage);
-
 
 module.exports = router;
