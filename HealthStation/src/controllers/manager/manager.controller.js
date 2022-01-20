a33e302e3cb4aeb025d6daa148c1707d4c705af4
ascorbic1230 <nguyenquoctoan1230@gmail.com>
2022-01-20 14:44:06 +0700

    some change

--- HealthStation/src/controllers/manager/manager.controller.js
+++ HealthStation/src/controllers/manager/manager.controller.js
@@ -1,170 +1,170 @@
 const utils = require('../../lib/utils')
 const fakePaymentData = [
-    {
-        name: "Sage Rodriguez",
-        debt: 50000,
-        status: "F0",
-    },
-    {
-        name: "Doris Greene",
-        debt: 10000,
-        status: "F1",
-    },
-    {
-        name: "Mason Porter",
-        debt: 0,
-        status: "F2",
-    },
-    {
-        name: "Jon Porter",
-        debt: 40000,
-        status: "F0",
-    },
+  {
+    name: "Sage Rodriguez",
+    debt: 50000,
+    status: "F0",
+  },
+  {
+    name: "Doris Greene",
+    debt: 10000,
+    status: "F1",
+  },
+  {
+    name: "Mason Porter",
+    debt: 0,
+    status: "F2",
+  },
+  {
+    name: "Jon Porter",
+    debt: 40000,
+    status: "F0",
+  },
 ];
 
 const fakeData = [
-    {
-        name: "Sage Rodriguez",
-        country: "Netherlands",
-        city: "Baileux",
-        status: "F0",
-    },
-    {
-        name: "Doris Greene",
-        country: "Malawi",
-        city: "Feldkirchen in Kärnten",
-        status: "F1",
-    },
-    {
-        name: "Mason Porter",
-        country: "Chile",
-        city: "Gloucester",
-        status: "F2",
-    },
-    {
-        name: "Jon Porter",
-        country: "Portugal",
-        city: "Gloucester",
-        status: "F0",
-    },
+  {
+    name: "Sage Rodriguez",
+    country: "Netherlands",
+    city: "Baileux",
+    status: "F0",
+  },
+  {
+    name: "Doris Greene",
+    country: "Malawi",
+    city: "Feldkirchen in Kärnten",
+    status: "F1",
+  },
+  {
+    name: "Mason Porter",
+    country: "Chile",
+    city: "Gloucester",
+    status: "F2",
+  },
+  {
+    name: "Jon Porter",
+    country: "Portugal",
+    city: "Gloucester",
+    status: "F0",
+  },
 ];
 
 const fakeManagementData = [
-    {
-        type: "Thêm mới",
-        description: "Thêm bệnh nhân A vào danh sách những người F0 ",
-        create_at: "22/12/2021",
-    },
-    {
-        type: "Thêm nhu yếu phẩm",
-        description: "Thêm Đường vào danh sách nhu yếu phẩm",
-        create_at: "22/12/2021",
-    },
-    {
-        type: "Xóa gói nhu yếu phẩm",
-        description: "Xóa gói hỗ trợ 69",
-        create_at: "22/12/2021",
-    },
-    {
-        type: "Chuyển nơi điều trị",
-        description: "Chuyền bệnh nhân X từ khu cách ly A sang khu cách ly B",
-        create_at: "22/12/2021",
-    },
-    {
-        type: "Xuất viện",
-        description: "Cho phép bệnh nhân A rời khỏi khu cách ly",
-        create_at: "22/12/2021",
-    },
+  {
+    type: "Thêm mới",
+    description: "Thêm bệnh nhân A vào danh sách những người F0 ",
+    create_at: "22/12/2021",
+  },
+  {
+    type: "Thêm nhu yếu phẩm",
+    description: "Thêm Đường vào danh sách nhu yếu phẩm",
+    create_at: "22/12/2021",
+  },
+  {
+    type: "Xóa gói nhu yếu phẩm",
+    description: "Xóa gói hỗ trợ 69",
+    create_at: "22/12/2021",
+  },
+  {
+    type: "Chuyển nơi điều trị",
+    description: "Chuyền bệnh nhân X từ khu cách ly A sang khu cách ly B",
+    create_at: "22/12/2021",
+  },
+  {
+    type: "Xuất viện",
+    description: "Cho phép bệnh nhân A rời khỏi khu cách ly",
+    create_at: "22/12/2021",
+  },
 ];
 
 const fakeProductChartData = {
-    dates: "['15-01-2022', '16-01-2022', '17-01-2022', '18-01-2022', '19-01-2022', '20-01-2022']",
-    data: [
-        {
-            name: "'Nước ngọt'",
-            data: '[2, 17, 10, 38, 27, 21]',
-            color: utils.randomColor()
-        },
-        {
-            name: "'Bim bim'",
-            data: '[8, 7, 25, 35, 5, 14]',
-            color: utils.randomColor()
-        },
-        {
-            name: "'Bánh ngọt'",
-            data: '[37, 29, 14, 15, 26, 23]',
-            color: utils.randomColor()
-        },
-        {
-            name: "'Mì tôm'",
-            data: '[1, 27, 36, 35, 12, 15]',
-            color: utils.randomColor()
-        },
-        {
-            name: "'Thịt bò'",
-            data: '[29, 26, 3, 37, 13, 1]',
-            color: utils.randomColor()
-        },
-        {
-            name: "'Thịt bò'",
-            data: '[19, 33, 34, 5, 1, 15]',
-            color: utils.randomColor()
-        },
-        {
-            name: "'Thịt bò'",
-            data: '[16, 36, 13, 1, 38, 18]',
-            color: utils.randomColor()
-        },
-    ]
+  dates: "['15-01-2022', '16-01-2022', '17-01-2022', '18-01-2022', '19-01-2022', '20-01-2022']",
+  data: [
+    {
+      name: "'Nước ngọt'",
+      data: '[2, 17, 10, 38, 27, 21]',
+      color: utils.randomColor()
+    },
+    {
+      name: "'Bim bim'",
+      data: '[8, 7, 25, 35, 5, 14]',
+      color: utils.randomColor()
+    },
+    {
+      name: "'Bánh ngọt'",
+      data: '[37, 29, 14, 15, 26, 23]',
+      color: utils.randomColor()
+    },
+    {
+      name: "'Mì tôm'",
+      data: '[1, 27, 36, 35, 12, 15]',
+      color: utils.randomColor()
+    },
+    {
+      name: "'Thịt bò'",
+      data: '[29, 26, 3, 37, 13, 1]',
+      color: utils.randomColor()
+    },
+    {
+      name: "'Thịt bò'",
+      data: '[19, 33, 34, 5, 1, 15]',
+      color: utils.randomColor()
+    },
+    {
+      name: "'Thịt bò'",
+      data: '[16, 36, 13, 1, 38, 18]',
+      color: utils.randomColor()
+    },
+  ]
 }
 
 const fakePackageChartData = {
-    dates: "['15-01-2022', '16-01-2022', '17-01-2022', '18-01-2022', '19-01-2022', '20-01-2022']",
-    data: [
-        {
-            name: "'Gói thực phẩm 1'",
-            data: '[1, 3, 10, 11, 14, 18]',
-            color: utils.randomColor()
-        },
-        {
-            name: "'Gói thực phẩm 2'",
-            data: '[2, 17, 12, 16, 19, 8]',
-            color: utils.randomColor()
-        },
-        {
-            name: "'Gói sinh hoạt cá nhân'",
-            data: '[11, 20, 4, 18, 5, 7]',
-            color: utils.randomColor()
-        },
-        {
-            name: "'Gói ăn vặt 1'",
-            data: '[2, 1, 11, 9, 8, 18]',
-            color: utils.randomColor()
-        },
-        {
-            name: "'Gói ăn vặt 2'",
-            data: '[12, 8, 18, 9, 10, 3]',
-            color: utils.randomColor()
-        },
-        {
-            name: "'Gói chống virus'",
-            data: '[19, 33, 34, 5, 1, 15]',
-            color: utils.randomColor()
-        },
-        {
-            name: "'Gói luxury'",
-            data: '[28, 22, 18, 21, 20, 37]',
-            color: utils.randomColor()
-        },
-    ]
+  dates: "['15-01-2022', '16-01-2022', '17-01-2022', '18-01-2022', '19-01-2022', '20-01-2022']",
+  data: [
+    {
+      name: "'Gói thực phẩm 1'",
+      data: '[1, 3, 10, 11, 14, 18]',
+      color: utils.randomColor()
+    },
+    {
+      name: "'Gói thực phẩm 2'",
+      data: '[2, 17, 12, 16, 19, 8]',
+      color: utils.randomColor()
+    },
+    {
+      name: "'Gói sinh hoạt cá nhân'",
+      data: '[11, 20, 4, 18, 5, 7]',
+      color: utils.randomColor()
+    },
+    {
+      name: "'Gói ăn vặt 1'",
+      data: '[2, 1, 11, 9, 8, 18]',
+      color: utils.randomColor()
+    },
+    {
+      name: "'Gói ăn vặt 2'",
+      data: '[12, 8, 18, 9, 10, 3]',
+      color: utils.randomColor()
+    },
+    {
+      name: "'Gói chống virus'",
+      data: '[19, 33, 34, 5, 1, 15]',
+      color: utils.randomColor()
+    },
+    {
+      name: "'Gói luxury'",
+      data: '[28, 22, 18, 21, 20, 37]',
+      color: utils.randomColor()
+    },
+  ]
 }
 
 const fakePaymentChartData = {
-    months: "['Tháng 6 - 2021', 'Tháng 7 - 2021', 'Tháng 8 - 2021', 'Tháng 9 - 2021', 'Tháng 10 - 2021', 'Tháng 11 - 2021', 'Tháng 12 - 2021', 'Tháng 1 - 2022']",
-    data: {
-        debt: "[16326000, 32883000, 41044000, 23759000, 37569000, 27091000, 34904000, 25865000]",
-        payment: "[63101000, 49837000, 23081000, 47989000, 22778000, 52021000, 29205000, 76020000]"
-    }
+  months: "['Tháng 6 - 2021', 'Tháng 7 - 2021', 'Tháng 8 - 2021', 'Tháng 9 - 2021', 'Tháng 10 - 2021', 'Tháng 11 - 2021', 'Tháng 12 - 2021', 'Tháng 1 - 2022']",
+  data: {
+    debt: "[16326000, 32883000, 41044000, 23759000, 37569000, 27091000, 34904000, 25865000]",
+    payment: "[63101000, 49837000, 23081000, 47989000, 22778000, 52021000, 29205000, 76020000]"
+  }
 }
 
 const categoryModel = require("../../models/sites/category.model");
@@ -179,8 +179,8 @@ const { uploadMultipleFiles, uploadFile, deleteFile } = require("../../config/fi
 
 const moment = require('moment')
 const {
-    updateStateOfAllRelated,
-    updateStateById,
+  updateStateOfAllRelated,
+  updateStateById,
 } = require("../../models/user/user.model");
 const { PERMISSIONS } = require('../../constants/index')
 const { callBankingApi } = require('../../lib/utils')
@@ -236,8 +236,8 @@ module.exports = {
       const user_id = (await userModel.findByUsername(user.username)).data
         .account_id;
 
-      await callBankingApi('/auth/register', "POST", { id: user_id})
-      
+      await callBankingApi('/auth/register', "POST", { id: user_id })
+
       for (let i = 0; i < related_id.length; i++) {
         await userModel.createRelated(user_id, related_id[i]);
         await userModel.createRelated(related_id[i], user_id);
@@ -245,43 +245,43 @@ module.exports = {
       return res.redirect('/manager');
     }
   },
-    
-  getAccount: async (req, res) => {
-        const data = await userModel.findAllPatient();
-        let response = await numPatientsLogModel.findAll();
-        response.data = response.data.slice(Math.max(response.data.length - 10, 0));
-        const chart_data = {
-            dates: [],
-            data: {
-                normal: [],
-                F0: [],
-                F1: [],
-                F2: [],
-                F3: []
-            }
-        };
-        for (let i = 0; i < response.data.length; i++) {
-            chart_data.dates.push(moment(response.data[i].date_created).format('DD-MM-YYYY'));
-            chart_data.data.normal.push(response.data[i].normal);
-            chart_data.data.F0.push(response.data[i].f0);
-            chart_data.data.F1.push(response.data[i].f1);
-            chart_data.data.F2.push(response.data[i].f2);
-            chart_data.data.F3.push(response.data[i].f3);
-        }
-        chart_data.dates = utils.arrayStringToString(chart_data.dates);
-        chart_data.data.normal = utils.arrayNumberToString(chart_data.data.normal);
-        chart_data.data.F0 = utils.arrayNumberToString(chart_data.data.F0);
-        chart_data.data.F1 = utils.arrayNumberToString(chart_data.data.F1);
-        chart_data.data.F2 = utils.arrayNumberToString(chart_data.data.F2);
-        chart_data.data.F3 = utils.arrayNumberToString(chart_data.data.F3);
 
-        res.render("layouts/manager/accountManagement", {
-            layout: "manager/main",
-            data: data.data,
-            chart_data: chart_data,
-            active: { accManagement: true },
-        });
-    },
+  getAccount: async (req, res) => {
+    const data = await userModel.findAllPatient();
+    let response = await numPatientsLogModel.findAll();
+    response.data = response.data.slice(Math.max(response.data.length - 10, 0));
+    const chart_data = {
+      dates: [],
+      data: {
+        normal: [],
+        F0: [],
+        F1: [],
+        F2: [],
+        F3: []
+      }
+    };
+    for (let i = 0; i < response.data.length; i++) {
+      chart_data.dates.push(moment(response.data[i].date_created).format('DD-MM-YYYY'));
+      chart_data.data.normal.push(response.data[i].normal);
+      chart_data.data.F0.push(response.data[i].f0);
+      chart_data.data.F1.push(response.data[i].f1);
+      chart_data.data.F2.push(response.data[i].f2);
+      chart_data.data.F3.push(response.data[i].f3);
+    }
+    chart_data.dates = utils.arrayStringToString(chart_data.dates);
+    chart_data.data.normal = utils.arrayNumberToString(chart_data.data.normal);
+    chart_data.data.F0 = utils.arrayNumberToString(chart_data.data.F0);
+    chart_data.data.F1 = utils.arrayNumberToString(chart_data.data.F1);
+    chart_data.data.F2 = utils.arrayNumberToString(chart_data.data.F2);
+    chart_data.data.F3 = utils.arrayNumberToString(chart_data.data.F3);
+
+    res.render("layouts/manager/accountManagement", {
+      layout: "manager/main",
+      data: data.data,
+      chart_data: chart_data,
+      active: { accManagement: true },
+    });
+  },
 
 
   getAccountDetails: async (req, res) => {
@@ -312,9 +312,9 @@ module.exports = {
       await quarantineLocationModel.findAll();
 
     locations = locations.map(location => {
-        if(location.num_patients < location.capacity || location.location_id == user.quarantine_location_id) {
-          return location
-        }
+      if (location.num_patients < location.capacity || location.location_id == user.quarantine_location_id) {
+        return location
+      }
     })
     const related_base_data = (await userModel.findAllPatientWithout(id)).data;
     const related_with_user = (await userModel.findAllRelatedById(id)).data;
@@ -442,20 +442,20 @@ module.exports = {
   // end category
 
   // start product
-    getProduct: async (req, res) => {
-        const { data } = await productModel.findAll();
-        for (const product of data) {
-            const category = await categoryModel.findById(product.category_id);
-            product.category_name = category.data.name;
-        }
+  getProduct: async (req, res) => {
+    const { data } = await productModel.findAll();
+    for (const product of data) {
+      const category = await categoryModel.findById(product.category_id);
+      product.category_name = category.data.name;
+    }
 
-        res.render("layouts/manager/productManagement", {
-            layout: "manager/main",
-            data: data,
-            chart_data: fakeProductChartData,
-            active: { proManagement: true },
-        });
-    },
+    res.render("layouts/manager/productManagement", {
+      layout: "manager/main",
+      data: data,
+      chart_data: fakeProductChartData,
+      active: { proManagement: true },
+    });
+  },
 
   getCreateProduct: async (req, res) => {
     const { data: categories } = await categoryModel.findAll();
@@ -500,18 +500,18 @@ module.exports = {
   },
   // end product
 
-    
-  // start package
-    getPackage: async (req, res) => {
-        const { data } = await packageModel.findAll();
 
-        res.render("layouts/manager/packageManagement", {
-            layout: "manager/main",
-            data: data,
-            chart_data: fakePackageChartData,
-            active: { packManagement: true },
-        });
-    },
+  // start package
+  getPackage: async (req, res) => {
+    const { data } = await packageModel.findAll();
+
+    res.render("layouts/manager/packageManagement", {
+      layout: "manager/main",
+      data: data,
+      chart_data: fakePackageChartData,
+      active: { packManagement: true },
+    });
+  },
 
   getCreatePackage: async (req, res) => {
     const { data: productsData } = await productModel.findAll();
@@ -595,23 +595,23 @@ module.exports = {
   // end package
 
   // start payment
-    getPayment: async (req, res, next) => {
-        const { data: amount } = await minimumPaymentModel.find();
+  getPayment: async (req, res, next) => {
+    const { data: amount } = await minimumPaymentModel.find();
 
-        res.render("layouts/manager/paymentManagement", {
-            layout: "manager/main",
-            amount: amount,
-            data: fakePaymentData,
-            chart_data: fakePaymentChartData,
-            active: { paymentManagement: true },
-        });
-    },
+    res.render("layouts/manager/paymentManagement", {
+      layout: "manager/main",
+      amount: amount,
+      data: fakePaymentData,
+      chart_data: fakePaymentChartData,
+      active: { paymentManagement: true },
+    });
+  },
 
-  
+
   updatePayment: async (req, res, next) => {
     const amount = req.body.amount;
     await minimumPaymentModel.update(amount);
     res.redirect('/manager/payment-management');
   },
-
+  // end payment
 };
