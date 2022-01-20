cf17d69ef43b8be0f40a25bb82ae85ba78c37583
Nguyen Hoang Tien <nguyenhoangtien100501@gmail.com>
2022-01-21 06:21:04 +0700

    add local version of final

--- HealthStation/src/config/db.js
+++ HealthStation/src/config/db.js
@@ -7,10 +7,7 @@ const conf = {
     database: process.env.DB_DATABASE,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    // ssl: false
-    ssl: {
-        rejectUnauthorized: false,
-    }
+    ssl: false
 }
 
 
