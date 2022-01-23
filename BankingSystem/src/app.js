3c974999c2b30fd6214cd821b64ee400821453e1
Nguyen Hoang Tien <nguyenhoangtien100501@gmail.com>
2022-01-23 14:35:49 +0700

    add PORT variable

--- BankingSystem/src/app.js
+++ BankingSystem/src/app.js
@@ -3,8 +3,9 @@ const passport = require('./middlewares/passport');
 const cors = require('cors');
 const route = require('./routes');
 const app = require("https-localhost")()
+require('dotenv').config();
 // const app = express()
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 
 app.use(express.urlencoded({ extended: true }));
